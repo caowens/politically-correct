066646ad5029660c978df223ccc1343fe651b925
caowens <caowens555@yahoo.com>
2022-10-26 14:13:27 -0500

    Add Card and Left components data functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,21 @@ export default function Card(props) {
   return (
     <>
       <div className={`card mb-3 ${props.border}`}>
-        <div class={`card-body ${props.text}`}>
-          <img src="https://media.istockphoto.com/photos/breaking-news-world-news-with-map-backgorund-picture-id1182477852?k=20&m=1182477852&s=612x612&w=0&h=I3wdSzT_5h1y9dHq_YpZ9AqdIKg8epthr8Guva8FkPA=" class="card-img-top" alt="..."></img>
-          <a href="#"><h5 class="card-title">Card title</h5></a>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <div class={`card-body ${props.textColor}`}>
+          <img
+            src={`${props.img}`}
+            class="card-img-top"
+            alt="..."
+          ></img>
+          <a href="#">
+            <h5 class="card-title">{props.title}</h5>
+          </a>
+          <p class="card-text">
+            {props.excerpt}
+          </p>
+          <p class="card-text">
+            <small class="text-muted">{props.source}</small>
+          </p>
         </div>
       </div>
     </>
--- src/pages/Left.jsx
+++ src/pages/Left.jsx
@@ -1,24 +1,68 @@
 import React from "react";
-import RowLayout from '../components/RowLayout';
-import Card from '../components/Card';
-import CardLong from '../components/CardLong';
+import RowLayout from "../components/RowLayout";
+import Card from "../components/Card";
+import CardLong from "../components/CardLong";
+import myData from "../data/news.json";
 
 export default function Left() {
+  // Have a list of 4 articles
+  // Pass that list to the row layout
+  // Then row layout makes as many cards as are in that list of 4 or less
+
+  // 1. Check if last item in array
+  // 2. If last item, push to row and break
+
+  const articles = [];
+  let i = 0;
+  let j = 0;
+  let row = [];
+  while (i < myData.articles.length) {
+    // Test if last item in the array (In the case that the row is less than 4)
+    if (myData.articles[i] === myData.articles[myData.articles.length - 1]) {
+      row.push(myData.articles[i]);
+      articles.push(row);
+      break;
+    }
+
+    // Each row will need 4 articles
+    else if (j < 4) {
+      row.push(myData.articles[i]);
+      j++;
+    } else {
+      j = 0;
+      articles.push(row);
+      row = [];
+    }
+    i++;
+  }
   return (
     <>
-        <div className='container-1'>
-        <div className='box-4'>
-          <Card border='border-primary' text='text-primary'/>
+      <div className="container-1">
+        <div className="box-4">
+          <Card
+            border="border-primary"
+            textColor="text-primary"
+            title={myData.articles[0].title}
+            img={myData.articles[0].media}
+            excerpt={myData.articles[0].excerpt}
+            source={myData.articles[0].clean_url}
+          />
         </div>
-        <div className='box-5'>
-          <CardLong border='border-primary' text='text-primary'/>
+        <div className="box-5">
+          <CardLong border="border-primary" text="text-primary" />
         </div>
-        <div className='box-6'>
-          <CardLong border='border-primary' text='text-primary'/>
+        <div className="box-6">
+          <CardLong border="border-primary" text="text-primary" />
         </div>
       </div>
-      <RowLayout border='border-primary' text='text-primary'/>
-      <RowLayout border='border-primary' text='text-primary'/>
+      {articles.map((item, key) => (
+        <RowLayout
+          row={item}
+          key={key}
+          border="border-primary"
+          textColor="text-primary"
+        />
+      ))}
     </>
   );
 }
